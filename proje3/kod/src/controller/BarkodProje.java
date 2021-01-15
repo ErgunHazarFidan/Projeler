16ad4c8e6d747a98d531440d5ce3b2158b31dd15
Ergün Hazar Fidan <77468325+ErgunHazarFidan@users.noreply.github.com>
2021-01-15 04:23:52 +0300

    Add files via upload

---
+++ proje3/kod/src/controller/BarkodProje.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import view.Login;
+
+
+public class BarkodProje {
+
+    
+    public static void main(String[] args) {
+       new Login().setVisible(true);
+        
+    }
+    
+}
